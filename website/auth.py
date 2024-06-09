0581d5b440062336cbf2cc8359828483cf791c05
Ali AB <aliabdev07@gmail.com>
2024-06-09 19:14:30 +0100

    Checking if User is Logged In

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -15,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged successfully', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, please try again.', category='error')
@@ -23,11 +25,13 @@ def login():
 
     '''data = request.form
     print(data)'''
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -52,6 +56,8 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+
+            login_user(user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
